af1ae46379be97b62983697cd010e8667c9eb62b
Elek Guidolin <elekguidolin@gmail.com>
2025-08-21 06:08:30 -0300

    Finalizando desafio!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+var iPhone = new Iphone("123", "I6", "jkjhkhj", 15);
+var nokia = new Nokia("456", "Qualquer", "jhkhkhkhkhkhk", 16);
+
+iPhone.Ligar();
+iPhone.ReceberLigacao();
+iPhone.InstalarAplicativo("Whats");
+
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Whats");
